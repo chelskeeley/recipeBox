9431afa54c083c1a77f0e5fdd30ad786c87da26d
Chelsea Keeley <chelsea.keeley@gmail.com>
2018-01-07 23:09:38 -0500

    created recipeList.js

---
+++ dev/scripts/components/recipeList.js
@@ -0,0 +1,11 @@
+import React from 'React';
+
+class recipeList extends React.Component {
+    render(){
+        return(
+            <div>
+                Recipe List
+            </div>
+        )
+    }
+}
