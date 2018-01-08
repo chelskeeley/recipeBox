74083507f6848d93ebaf918d67b075e45a47496a
Chelsea Keeley <chelsea.keeley@gmail.com>
2018-01-08 12:44:41 -0500

    created homepage.js and header.js

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router, Route, Link
+} from 'react-router-dom';
+import Header from './components/header.js'
 
 class App extends React.Component {
     render() {
       return (
-        <div>
-          Recipe Box
+        <Router>
+          <div>
+            <Header />
+              <Switch>
+                <Route exact path='/' component='HomePage'></Route>
+              </ Switch>
+            <footer>
+              <p>&copy; 2018 Chelsea Keeley</p>
+            </footer>
         </div>
+          </Router>
       )
     }
 }
